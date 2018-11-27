6ed38909382cc1046372980a3db0547582d0b7ce
Victor Guadarrama <victor.gm@eucledian.com>
2018-11-27 16:46:24 -0600

    Added Bootstrap, User & Project Controllers/Models/Views, Project DB Seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_user
+    User.new(session[:id])
+  end
+
+  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'project/index'
+  root to: 'pages#index'
+  resources :sessions, only: [:update]
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
